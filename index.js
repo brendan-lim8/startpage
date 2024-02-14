c569afc7219dd3e73797a4c20155201dc89a63b0
Brendeen8 <159737193+Brendeen8@users.noreply.github.com>
2024-02-13 17:37:24 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 function showTime() {
-	const date = new Date();
+    const date = new Date();
 
-	let today = date.toLocaleString("en", { weekday: "long" });
-	let hour = date.toLocaleString("pl", { hour: "2-digit", hour12: true });
-	let minute = date.toLocaleString("en", { minute: "2-digit" });
-	let second = date.toLocaleString("en", { second: "2-digit" });
-	let day = date.toLocaleString("en", { day: "2-digit" });
-	let month = date.toLocaleString("en", { month: "2-digit" });
-	let year = date.toLocaleString("en", { year: "numeric" });
+    let today = date.toLocaleString("en", { weekday: "long" });
+    let time = date.toLocaleString("en", { hour: "2-digit", minute: "2-digit", second: "2-digit", hour12: true });
+    let day = date.toLocaleString("en", { day: "2-digit" });
+    let month = date.toLocaleString("en", { month: "2-digit" });
+    let year = date.toLocaleString("en", { year: "numeric" });
 
 	minute = addZero(minute);
 	second = addZero(second);
